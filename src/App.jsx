601451a4a594f4ba9d119ce84904c539ab4fa811
Giancarlo <cooper458.gm@gmail.com>
2026-02-10 04:25:15 +0100

    added function and return

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,36 @@
 import { useState } from "react";
-import languages from "./data/languages";
+import languages from "../languages";
+
+function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState(null);
+
+  return (
+    <div>
+      <h1>Linguaggi Web</h1>
+
+      <div>
+        {languages.map((language) => (
+          <button
+            key={language.id}
+            onClick={() => setSelectedLanguage(language)}
+          >
+            {language.title}
+          </button>
+        ))}
+      </div>
+
+      <div>
+        {selectedLanguage ? (
+          <>
+            <h2>{selectedLanguage.title}</h2>
+            <p>{selectedLanguage.description}</p>
+          </>
+        ) : (
+          <p>Nessun linguaggio selezionato</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
